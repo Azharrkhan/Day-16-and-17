e07cedd0283be092b3592681c8053e35b16e24a7
Azhar <azharumi786@gmail.com>
2022-10-03 16:03:36 +0530

    added bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,28 +2,30 @@ package com.bl.dsa;
 
 public class BubbleSort {
 	    public static void main(String[] args) {
-	        String[] bubble = {"Azhar", "Simran", "Simran", "Muskan", "Afifa"};
-
+	        String[] a = {"Azhar", "Simran", "Simran", "Muskan", "Afifa"};
+//	        int [] a = {5, 25, 12, 45, 100, 58, 79};
 	        //bubble sort
-	        for(int i = 0; i < bubble.length - 1; i++)
+	        for(int i = 0; i < a.length - 1; i++)
 	        {
-	            for(int j = 0; j < bubble.length -i -1; j++)
+	            for(int j = 0; j < (a.length -i -1); j++)
 	            {
-	                if(bubble[j].compareTo(bubble[j + 1]) > 0){
-	                    String temp = bubble[j];
-	                    bubble[j] = bubble[j + 1];
-	                    bubble[j + 1] = temp;
+//	                if(a[j] > a[j + 1]){
+	                if(a[j].compareTo(a[j+1]) > 0) {
+//	                    int temp = a[j];
+	                    String temp = a[j];
+	                    a[j] = a[j + 1];
+	                    a[j + 1] = temp;
 	                }
 	            }
 	        }
-	        printBubble(bubble);
+	        printBubble(a);
 	    }
-
-	    public static void printBubble(String[] bubble){
+//	    public static void printBubble(int[] a){
+	    public static void printBubble(String[] a){
 	        System.out.println("Bubble sorted array : ");
-	        for(int i = 0; i < bubble.length; i++)
+	        for(int i = 0; i < a.length; i++)
 	        {
-	            System.out.println(bubble[i]);
+	            System.out.println(a[i]);
 	        }
 	    }
 }
